980e76046da20413f262ffaa792cb9bfc419963e
PANKYAAA <pankajs.gaikar@gmail.com>
2020-03-22 18:30:27 +0530

    - Created initial project structure with view classes.

---
+++ MusicBrowser/View/ArtistAlbumsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ArtistAlbumsViewController.swift
+//  MusicBrowser
+//
+//  Created by Pankaj Gaikar on 22/03/20.
+//  Copyright © 2020 Pankaj Gaikar. All rights reserved.
+//
+
+import UIKit
+
+class ArtistAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MusicBrowser/View/ArtistsSearchViewController.swift
+++ MusicBrowser/View/ArtistsSearchViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ArtistsSearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
